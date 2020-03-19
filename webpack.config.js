69c2d1c35c97a8e80750e5e1afff9370fe3e684a
Sely <lxh@joinforwin.com>
2020-03-19 18:05:33 +0800

    压缩js以及css

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,9 @@
 let path = require("path"); //自带的路径插件
-let htmlWebpackPlugin = require("html-webpack-plugin"); // html插件
+let HtmlWebpackPlugin = require("html-webpack-plugin"); // html插件
+let MiniCssExtractPlugin = require("mini-css-extract-plugin"); // 将样式文件抽离成main.css文件 
+//将css压缩，需要插件optimize-css-assets-webpack-plugin 处理它 需要插件terser-webpack-plugin
+let OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin'); //优化css
+let TerserJSPlugin = require('terser-webpack-plugin');
 
 module.exports = {
   devServer: {
@@ -10,7 +14,19 @@ module.exports = {
     open: true, //启动浏览器
     compress: true //gzip 压缩
   },
-  mode: "development", //模式 默认两种 production 生产模式 development 开发模式
+  optimization: {//优化项
+    minimizer: [
+      new TerserJSPlugin({}), //优化js
+      new OptimizeCSSAssetsPlugin({})//优化css
+    ],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: '[name].css',
+      chunkFilename: '[id].css',
+    }),
+  ],
+  mode: "production", //模式 默认两种 production 生产模式 development 开发模式
   entry: "./src/index.js", //入口 从那个地方开始打包
   output: {
     //出口
@@ -19,15 +35,13 @@ module.exports = {
   },
   plugins: [
     //数组，放着所有webpack的插件
-    new htmlWebpackPlugin({
+    new HtmlWebpackPlugin({
       template: "./src/index.html", //模板
       filename: "index.html", //打包后的文件名
-      minify: {
-        //打包压缩
-        removeAttributeQuotes: true, //删除双引号
-        collapseWhitespace: true //打包成一行
-      },
       hash: true //hash戳
+    }),
+    new MiniCssExtractPlugin({
+      filename: "main.css" //打包后的文件名
     })
   ],
 
@@ -43,26 +57,17 @@ module.exports = {
       //loader还可以写成 对象方式
       {
         test: /\.css$/,
-        use: [
-          {
-            loader: "style-loader",
-            options: {
-              insert: "top" //将html中的style提前
-            }
-          },
-          "css-loader"
-        ]
+        use: [MiniCssExtractPlugin.loader, "css-loader", "postcss-loader"]
       },
       {
-        //可以处理less文件 sass stylus 
-        //sass node-sass sass-loader 
+        //可以处理less文件 sass stylus
+        //sass node-sass sass-loader
         //stylus stylus-loader
         test: /\.less$/,
         use: [
-          {
-            loader: "style-loader"
-          },
+          MiniCssExtractPlugin.loader,
           "css-loader", //@import 解析路径
+          "postcss-loader",
           "less-loader" // less => css
         ]
       }
